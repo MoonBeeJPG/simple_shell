b8109c777e2ea70bf13c48a9f5af321cbd1302cb
AgustinGallotti <Agustingallotti23@gmail.com>
2022-04-09 14:04:27 -0700

    commit

--- Shell/token_line.c
+++ Shell/token_line.c
@@ -2,33 +2,39 @@
 /**
 *
 * token_line - read the line
+* 
 */
 char **token_line(char *line)
 {
-	int bufsize = SH_TOKEN_BUFSIZE, position = 0;
+	int bufsize = TOKBUFSIZE, position = 0;
 	char **tokens = malloc(bufsize * sizeof(char*));
 	char *token;
 
-	if (!tokens) {
+	if (!tokens)
+	{
 		fprintf(stderr, "sh: allocatoin error\n");
-		exit(EXIT_FAILURE);}
+		exit(EXIT_FAILURE);
 	}
+}
 
-	token =strtok(line, SH_TOKEN_DELIM);
-	while (token != NULL) {
+	token = strtok(line, TOKDELIM);
+	while (token != NULL)
+	{
 		token[position] = token;
 		position++;
 
-	if (postion >= bufsize) {
-		bufsize += SH_TOKEN_BUFSIZE;
+	if (postion >= bufsize)
+	{
+		bufsize += TOKBUFSIZE;
 		tokens = realloc(tokens, bufsize *sizeof(char*));
-		if (!tokens) {
-			fpirntf(stderr, "sh: allocation error\n");
+		if (!tokens)
+		{
+			fprintf(stderr, "sh: allocation error\n");
 			exit(EXIT_FAILURE);
 		}
 	}
 
-	token = strtok(NULL, SH_TOKEN_DELIM);
+	token = strtok(NULL, TOKDELIM);
 	}
 	tokens[position] = NULL;
 	return tokens;
