99fd8797830a03b85ca0b1bc1222ca808521e1bf
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-04-09 14:51:41 -0700

    readline.c

--- Shell/readline.c
+++ Shell/readline.c
@@ -1,20 +1,22 @@
 #include "main.h"
 /**
-* read_line - function that read your command
+* read_line - function that read the input line 
+*
+* line: input line
+* bufsize: size of the malloc that getline automatically creates
+*
+* Return: the input line already read
 */
 char *read_line(void)
 {
 	char *line = NULL;
-  ssize_t bufsize = 0; // have getline allocate a buffer for us
+	ssize_t bufsize = 0;
 
-  if (getline(&line, &bufsize, stdin) == -1){
-    if (feof(stdin)) {
-      exit(EXIT_SUCCESS);  // We recieved an EOF
-    } else  {
-      perror("readline");
-      exit(EXIT_FAILURE);
-    }
-  }
-
-  return line;
+	if (getline(&line, &bufsize, stdin) == -1)
+	{
+		perror("readline");
+		exit(EXIT_FAILURE);
+	}
+	
+	return line;
 }
