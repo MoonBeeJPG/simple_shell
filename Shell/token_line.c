4e2cb71fa7afd1afeeb9e651ffb271f9bcebff96
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-04-09 14:33:17 -0700

    token_line.c

--- Shell/token_line.c
+++ Shell/token_line.c
@@ -1,41 +1,51 @@
 #include "main.h"
+#define TOKBUFSIZE 64
+#define TOKDELIM "/t/r/n/a"
 /**
-*
 * token_line - read the line
 * 
+* @line: previus input line
+*
+* bufsize: capacity of the buffer
+* position: always positioned in the beginning of the buffer at startup 
+* buff: capacity for the tokenized line memory allocation
+* token: tokenization of the input line
+*
+* Return: buff with the tokenized line successfuly allocated
 */
 char **token_line(char *line)
 {
 	int bufsize = TOKBUFSIZE, position = 0;
-	char **tokens = malloc(bufsize * sizeof(char*));
+	char **buff = malloc(bufsize * sizeof(char *));
 	char *token;
 
-	if (!tokens)
+	if (!buff)
 	{
-		fprintf(stderr, "sh: allocatoin error\n");
+		perror(stderr, "shell: allocation error\n");
 		exit(EXIT_FAILURE);
 	}
-}
 
 	token = strtok(line, TOKDELIM);
+	
 	while (token != NULL)
 	{
 		token[position] = token;
 		position++;
 
-	if (postion >= bufsize)
-	{
-		bufsize += TOKBUFSIZE;
-		tokens = realloc(tokens, bufsize *sizeof(char*));
-		if (!tokens)
+		if (position >= bufsize)
 		{
-			fprintf(stderr, "sh: allocation error\n");
-			exit(EXIT_FAILURE);
+			bufsize += TOKBUFSIZE;
+			buff = realloc(buff, bufsize * sizeof(char *));
+			
+			if (!buff)
+			{
+				perror(stderr, "shell: allocation error\n");
+				exit(EXIT_FAILURE);
+			}
 		}
+		token = strtok(NULL, TOKDELIM);
 	}
 
-	token = strtok(NULL, TOKDELIM);
-	}
-	tokens[position] = NULL;
-	return tokens;
+	buff[position] = NULL;
+	return buff;
 }
