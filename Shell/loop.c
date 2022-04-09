381fd3b0b2b7032edc5d3d1eebf078e0a763aeee
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-04-09 13:37:56 -0700

    loop.c

--- Shell/loop.c
+++ Shell/loop.c
@@ -1,18 +1,26 @@
 #include "main.h"
-
-void sh_loop(void)
+/**
+* loop - Infinite loop that are always on when the shell runs
+* 
+* @line: the input, when the person writes something this is the line reading
+* @tok: the previous line tokenized
+* @func: the program corresponding to the input read and tokenized
+*
+*/
+void loop(void)
 {
 	char *line;
-	char **args;
-	int status;
+	char **tok;
+	int func;
 
-	do {
+	do 
+	{
 		printf("#cisfun$ ");
-		line = sh_read_line();
-		args = sh_token_line(line);
-		status = sh_execute(args);
+		line = readline();
+		tok = token_line(line);
+		func = match(tok);
 
 		free(line);
-		free(args);
-	}	while (status);
+		free(tok);
+	} while (func);
 }
