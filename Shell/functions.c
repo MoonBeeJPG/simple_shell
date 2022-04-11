3cae9b339f06fc1a1daf6f716c5c00b068de2b2e
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-04-11 11:30:01 -0700

    execve

--- Shell/functions.c
+++ Shell/functions.c
@@ -109,27 +109,27 @@ int lsh_launch(char **args)
 {
     pid_t pid, wpid;
     int status;
-
-        pid = fork();
-        if (pid == 0)
+    
+	pid = fork();
+	if (pid == 0)
+    {
+    	if (execvp(args[0], args) == -1)
         {
-            if (execve(args[0], args, 0) == -1)
-            {
-                perror("lsh");
-            }
-                exit(EXIT_FAILURE);
+        	perror("lsh");
         }
-        else if (pid < 0)
-        {
-            perror("lsh");
-        }
-        else
-        {
-            do {
-                wpid = waitpid(pid, &status, WUNTRACED);
-            } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-        }
-        return (1);
+        exit(EXIT_FAILURE);
+    }
+    else if (pid < 0)
+    {
+    	perror("lsh");
+    }
+    else
+    {
+    	do {
+        	wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    return (1);
 }
 
 /**
