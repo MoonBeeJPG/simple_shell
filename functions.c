2769ab4e3f40d74e2567d9f3c0abcaa7d1866fd1
AgustinGallotti <Agustingallotti23@gmail.com>
2022-04-12 11:55:14 -0700

    checking

--- functions.c
+++ functions.c
@@ -41,16 +41,13 @@ char *readline(void)
 	char *line = NULL;
 	size_t buffer = 0;
 
-	if (getline(&line, &buffer, stdin) == '\n')
+	if (getline(&line, &buffer, stdin) == -1)
 	{
 		exit(EXIT_FAILURE);
 	}
-	while (line[0] == '\n')
-		continue;
-/**	
 	while (checkinput(line) == -1)
 		continue;
-**/
+
 	return (line);
 }
 
