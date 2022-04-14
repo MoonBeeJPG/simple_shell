bd0412ca229fa66bf91b97bdc09fe249d2d826d8
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-04-13 19:55:19 -0700

    betty check

--- ctrl_c.c
+++ ctrl_c.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _ctrl_c - function that ignores ctrl + c input from keybord and prints a
- * break line with $ symbol.
+ * signalctrl_c - function that ignores ctrl + c input from
+ * keybord and prints a break line with $ symbol.
  * @signal: signal to ignore
  **/
 
